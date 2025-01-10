8b5b893d0e487e0873a379c50a65068e78c3ed56
Mohdhussain28 <md28md.hussain@gmail.com>
2025-01-11 02:49:43 +0530

    refract mongodb schema

--- models/cryptoModel.js
+++ models/cryptoModel.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const cryptoSchema = new mongoose.Schema({
-    coinId: String,
-    name: String,
-    currentPrice: Number,
-    marketCap: Number,
-    change24h: Number,
+    coinId: { type: String, required: true },
+    name: { type: String, required: true },
+    currentPrice: { type: Number, required: true },
+    marketCap: { type: Number },
+    change24h: { type: Number },
     fetchedAt: { type: Date, default: Date.now },
 });
 
